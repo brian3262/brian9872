e910148ed615eddf40850c681cb10024be4ef74b
brian3262 <90750492+brian3262@users.noreply.github.com>
2021-09-29 15:32:19 +0800

    Update examples 1.js

--- examples 1.js
+++ examples 1.js
@@ -1,22 +1,31 @@
-//VS PPT: https://drive.google.com/file/d/1pjOZp8Xp6Ghb31Nfnj0YRsVwAzMMyhCq/view?usp=sharing
-//JS PPT: https://drive.google.com/open?id=0BwN5VWV_C7-5ZnE3d045YWZJb00
-//Javascript 變數的使用
-
-var weight=50.5;
-var height=160;
-var bmi = weight/((height/100)**2);
-
-console.log("Hello! Your BMI value is "+ bmi);
-
-//使用第三方套件 readline-sync (待使用者輸入資料後程式再繼續執行) 
-//NodeJS 套件管理程式為 npm，需打開終端機輸入指令
-//安裝 readline-sync 套件指令: npm install readline-sync
-//另可透過package.json指定所有相關套件後 npm install 即可安裝所有套件
-
 const readline = require('readline-sync');
 weight = readline.question('請輸入您的體重(kg)?');
 height = readline.question('請輸入您的身高(cm)?');  
+weight = Number(weight)
+height = Number(height)
+
+//判斷數值是否不合理
 
 
-//not only bmi value, but also judgement
-//Ask user to input height only, is it workable?
+if(isNaN(weight)||(weight<=10 ||weight>=200))
+{
+console.log("你輸入的資料有誤! 請輸入介於10~200之間的數字");
+}
+else if ( isNaN(height) || height <= 100 || height > 220)
+{
+console.log("你輸入的資料有誤! 請輸入介於100~220之間的數字");
+} else {
+
+    height=(height/100);
+
+    var bmi=weight/((height)**2);
+    
+    var idealLow =18.5*(height**2);
+
+
+    var idealHi= 24* (height**2);
+
+console.log("你的理想體重範圍為"+idealLow+"~"+idealHi)
+
+console.log("你的BMI結果是:"+ bmi);
+}
