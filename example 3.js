9c0855cf9cd7f213805cb6ae0fd2655c8b6386f2
brian3262 <90750492+brian3262@users.noreply.github.com>
2021-10-13 15:35:45 +0800

    Update example 3.js

--- example 3.js
+++ example 3.js
@@ -1,1 +1,56 @@
+var poker=[];
+for(var i=0, i<52 , i++)
+switch (praseInt(i/13))
+{
+    case 0
+    
+    break;
+    case 1
+    
+    break;
+    
+    case 2
+    
+    break;
+    case 3
+    
+    break;
+    
+}
 
+
+for(let i=0, i<poker.length ;)
+{
+  var rand=math.floor(Math.random()*poker.length);
+  Console.log(poker[rand]);
+  Player1.push(poker.[rand])
+  poker.splice(rand,1);
+  
+}
+
+for(let i=0, i<poker.length ;)
+{
+  var rand=math.floor(Math.random()*poker.length);
+  Console.log(poker[rand]);
+  Player2.push(poker.[rand])
+  poker.splice(rand,1);
+  
+}
+
+for(let i=0, i<poker.length ;)
+{
+  var rand=math.floor(Math.random()*poker.length);
+  Console.log(poker[rand]);
+  Player3.push(poker.[rand])
+  poker.splice(rand,1);
+  
+}
+
+for(let i=0, i<poker.length ;)
+{
+  var rand=math.floor(Math.random()*poker.length);
+  Console.log(poker[rand]);
+  Player4.push(poker.[rand])
+  poker.splice(rand,1);
+  
+}
